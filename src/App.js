06774e71dafb55646fceac4edf12558c54b8adab
Rammohan Penchala <rpenchala@libertyfoxtech.com>
2025-06-03 12:23:19 -0400

    Add Footer component and integrate it into the App

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import './styles.css';
 
 import Header from './components/Header';
+import Footer from './components/Footer';
 
 function App() {
   return (
@@ -10,9 +11,7 @@ function App() {
       <div className="container">
         <Header></Header>
       </div>
-      <footer className="footer">
-        <p>&copy; 2025 Moviedux. All rights reserved.</p>
-      </footer>
+       <Footer></Footer>
     </div>
   );
 }
